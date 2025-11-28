982e88aadd31dd50ef84e0d823a884fa6513da93
Carlos Henrique <krlos.henrique98@gmail.com>
2025-11-28 18:01:48 -0300

    config: some config

---
+++ main.py
@@ -0,0 +1,5 @@
+print("=" * 80)
+print("No main.py was found so one was created!")
+print("Add your own code to this file: /home/henrique/Desktop/Curso Web FrontEnd/main.py")
+print("=" * 80)
+input("Key enter to close.")
